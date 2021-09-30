85e3286308c0c075c2962748cdf98746e2e8cf2d
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-30 15:15:55 +0200

    reorder modules and their contents using jquery-ui

--- courses/views.py
+++ courses/views.py
@@ -10,8 +10,26 @@ from django.contrib.auth.mixins import LoginRequiredMixin,\
     PermissionRequiredMixin
 from django.apps import apps
 from django.forms import modelform_factory
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin,
+                       JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
 
 
 class ModuleContentListView(TemplateResponseMixin, View):
