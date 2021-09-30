0b8f580e4a71635ed49755b13e7e52760381ddb9
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-30 15:29:14 +0200

    create course catalog

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from courses.views import CourseListView
 
 
 urlpatterns = [
@@ -27,6 +28,7 @@ urlpatterns = [
          auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
--- courses/views.py
+++ courses/views.py
@@ -9,10 +9,34 @@ from django.utils.text import slugify
 from django.contrib.auth.mixins import LoginRequiredMixin,\
     PermissionRequiredMixin
 from django.apps import apps
+from django.db.models import Count
+from django.views.generic.detail import DetailView
 from django.forms import modelform_factory
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses'))
+        courses = Course.objects.annotate(
+            total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
 
 class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
