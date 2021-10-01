add84de20ce805b0584edc2ae50d8271ca27cd34
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-10-01 13:58:21 +0200

    build api

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,13 +1,12 @@
-from django.views.generic import detail
 from rest_framework import generics, viewsets
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
 from rest_framework.decorators import action, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from ..models import Course, Subject
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import CourseWithContentsSerializer, SubjectSerializer, CourseSerializer, \
+    CourseWithContentsSerializer
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -34,3 +33,20 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         elif request.method == 'DELETE':
             course.students.remove(request.user)
         return Response({'success': True})
+
+    @action(detail=True, methods=['get'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def status(self, request, *args, **kwargs):
+        if self.get_object().students.filter(id=request.user.id) \
+                .exists():
+            return Response({'enrolled': True})
+        else:
+            return Response({'enrolled': False})
+
+    @action(detail=True, methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
